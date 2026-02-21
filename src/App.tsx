ce9467a05a52aefa437a37c2f5aedc14ad020ce8
Forklein <giuseppepisani2@gmail.com>
2026-02-21 14:32:16 +0100

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import Hero from './components/hero';
-import Navbar from './components/navbar';
-
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+import Skills from "./components/skills";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Skills />
     </div>
   )
 }
